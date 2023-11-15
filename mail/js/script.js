51d7b0d6743fad57c648a26357677a1fbbc4d484
Youss0203 <youssefhalibi003@gmail.com>
2023-11-15 15:18:23 +0100

    aggiunto array

---
+++ mail/js/script.js
@@ -0,0 +1,18 @@
+const newArray=[
+    "marcorossi@st.it",
+    "francoverde@st.it",
+    "gianmarcosbocco@st.it",
+    "cristianolarosa@st.it",
+    "francescomarzano@st.it",
+    "giorgiopower@st.it",
+    "alessandroblu@st.it",
+    "thomasgrigio@st.it"
+]
+
+const utente = prompt("scrivi la tua mail")
+
+for(let i=0; newArray.length;i++){
+    
+}
+
+
