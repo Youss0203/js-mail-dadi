765d2b32acdc29bcedec637480793273e45bca28
Youss0203 <youssefhalibi003@gmail.com>
2023-11-16 09:44:06 +0100

    aggiunte due piccole correzione nel gioco dei dadi

--- gioco dei dadi/js/script.js
+++ gioco dei dadi/js/script.js
@@ -2,9 +2,12 @@
 
  const userNumber = Math.floor(Math.random() * 6) + 1;
  const computerNumber = Math.floor(Math.random() * 6 )+ 1;
+ console.log(userNumber,computerNumber)
 
  if (userNumber > computerNumber) {
    console.log(`user win`);
  } else if (computerNumber > userNumber) {
    console.log(`computer win`);
+ }else{
+  console.log(`no one win`)
  }
